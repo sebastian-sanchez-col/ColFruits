dd6e6eb3af8244dd1d10faa37312f4222a5d87c2
Juan Sebastian Sanchez <juan2.sanchez@scotiabankcolpatria.com>
2023-05-05 11:28:06 -0500

    Initial Commit

---
+++ ColFruits/ColFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColFruitsApp.swift
+//  ColFruits
+//
+//  Created by Juan Sebastian Sanchez Mancilla on 5/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ColFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColFruits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColFruits
+//
+//  Created by Juan Sebastian Sanchez Mancilla on 5/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
