ce11877d0c8c4fe6cbe0570d31f3a441773b4f84
Juan Sanchez <jussanchezm@correo.udistrital.edu.co>
2024-05-10 16:22:47 -0700

    feature: creates fruit view

---
+++ ColFruits/Features/FruitsDescription/FruitListView.swift
@@ -0,0 +1,44 @@
+//
+//  FruitView.swift
+//  ColFruits
+//
+//  Created by Juan Sebastian Sanchez Mancilla on 5/05/23.
+//
+
+import SwiftUI
+
+struct FruitListView: View {
+    @StateObject var fruitList = FruitList()
+    
+    @EnvironmentObject var store: AppStore
+    
+    var body: some View {
+        VStack {
+            Text("List of Fruits")
+                .font(.title)
+                .padding()
+            
+            ForEach(Array(store.state.fruits.enumerated()), id: \.element.id) { index, fruit in
+                FruitCardView(fruit: fruit).accessibilityIdentifier("Fruit-\(fruit.id)")
+            }
+            .accessibilityIdentifier("FruitViewHStackItems")
+            
+        }.onAppear {
+            fruitList.fetchFruitsFromFile { result in
+                switch result {
+                case .success(let fruits):
+                    store.dispatch(.setFruits(fruits))
+                case .failure(let error):
+                    // TODO: - @ataches, Create error view -
+                    break
+                }
+            }
+        }
+    }
+}
+
+struct FruitView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitListView()
+    }
+}
--- ColFruits/Features/FruitsDescription/FruitView.swift
+++ ColFruits/Features/FruitsDescription/FruitView.swift
@@ -7,41 +7,111 @@
 
 import SwiftUI
 
-
-struct FruitView: View {
-    @StateObject var fruitList = FruitList()
-    
+struct FruitCardView: View {
     @EnvironmentObject var store: AppStore
+    private var fruit: Fruit
+    private var frameWidth: CGFloat?
+    private var frameHeight: CGFloat?
+    private var shouldUseFrame: Bool
+    private var fontSize: CGFloat
+    private var imageURL: String = ""
+    
+    init(
+        fruit: Fruit,
+        frameWidth: CGFloat? = 140,
+        frameHeight: CGFloat? = 128,
+        shouldUseFrame: Bool = true,
+        fontSize: CGFloat = 16
+    ) {
+        self.fruit = fruit
+        self.frameWidth = frameWidth
+        self.frameHeight = frameHeight
+        self.shouldUseFrame = shouldUseFrame
+        self.fontSize = fontSize
+        if let imageURL: String = fruit.imageURL {
+            self.imageURL = imageURL
+        }
+    }
+    
+    func isSelected() -> Bool{
+        return store.state.selectedFruit?.id == fruit.id
+    }
     
     var body: some View {
-        VStack {
-            Text("List of Fruits")
-                .font(.title)
-                .padding()
+        VStack(alignment: .leading) {
+            AsyncImage(url: URL(string: self.imageURL)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(7)
+            } placeholder: {
+                ProgressView()
+            }
+            .padding(.bottom, 7)
             
-            ForEach(Array(store.state.fruits.enumerated()), id: \.element.id) { index, fruit in
-                Text(fruit.name)
-                    .accessibilityIdentifier("Fruit-\(fruit.id)")
+            Text(self.fruit.name)
+                .font(.system(size: fontSize, weight: .regular))
+                .frame(alignment: .leading)
+        }
+        .frame(width: frameWidth, height: frameHeight)
+        .customFruitCardModifier(useFrame: shouldUseFrame)
+        .scaleEffect(isSelected() ? 1.03 : 1)
+        .animation(.linear)
+        .onTapGesture {
+            // TODO: - @ataches: store selected fruit -
+        }
+    }
+    
+    func getRandomFruitForComputer(idPlayerFruit: String, allFruits: [Fruit]) -> Fruit? {
+        return allFruits.filter { $0.id != idPlayerFruit }.randomElement()
+    }
+}
 
-            }
-            .accessibilityIdentifier("FruitViewHStackItems")
-            
-        }.onAppear {
-            fruitList.fetchFruitsFromFile { result in
-                switch result {
-                case .success(let fruits):
-                    store.dispatch(.setFruits(fruits))
-                case .failure(let error):
-                    // TODO: - @ataches, Create error view -
-                    break
-                }
-            }
+struct CustomFruitCardModifier: ViewModifier {
+    var useFrame: Bool
+    
+    func body(content: Content) -> some View {
+        if useFrame {
+            content
+                .frame(width: 150, height: 139)
+                .background(Color.white)
+                .cornerRadius(7)
+                .shadow(color: .black.opacity(0.25), radius: 7, x: -2, y: 3)
+                .padding([.horizontal, .bottom], 7)
+                .padding(.top, 6)
+        } else {
+            content
         }
     }
 }
 
-struct FruitView_Previews: PreviewProvider {
-    static var previews: some View {
-        FruitView()
+// TODO: - @ataches: relocate this view-
+extension View {
+    func customFruitCardModifier(useFrame: Bool) -> some View {
+        modifier(CustomFruitCardModifier(useFrame: useFrame))
     }
 }
+
+#if !TESTING
+struct FruitCardView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        let Fruits = [Fruit]
+
+        let previewStore: AppStore = {
+            let store = AppStore.preview
+            store.dispatch(.setFruits(Fruits))
+            
+            return store
+        }()
+
+        ZStack{
+            Color("BackgroundColor").ignoresSafeArea()
+            // TODO: - @ataches: fix [], change it for getElementAt -
+            FruitCardView(fruit: Fruits[0])
+                .previewLayout(.sizeThatFits)
+                .environmentObject(previewStore)
+        }
+    }
+}
+#endif
